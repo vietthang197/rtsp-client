d9d0904e1cacede0ac2f78e56dc47d2f5f1b397b
Lê Việt Thắng <levietthang1997@gmail.com>
2020-06-08 11:10:42 +0700

    Initial commit

---
+++ src/main/java/com/pro/rtspclient/RtspClientApplication.java
@@ -0,0 +1,13 @@
+package com.pro.rtspclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtspClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtspClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pro/rtspclient/RtspClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.rtspclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtspClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
